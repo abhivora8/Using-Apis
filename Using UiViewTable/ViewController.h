a81ce60c8e5e43e3b12476a4f71aac69bc741564
Abhishek Vora <abhivora8@gmail.com>
2016-06-05 15:00:06 +0530

    Initial Commit

---
+++ Using UiViewTable/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Using UiViewTable
+//
+//  Created by Abhishek Vora on 05/06/16.
+//  Copyright © 2016 Abhishek Vora. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Using UiViewTable/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Using UiViewTable
+//
+//  Created by Abhishek Vora on 05/06/16.
+//  Copyright © 2016 Abhishek Vora. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
